46db7a39174fc24bcc6493004468b508f568f627
Kacper Grzybkowski <kacperg@gmail.com>
2021-01-04 00:24:19 +0100

    #3 zadanie 2 podzielne przez 5

--- src/petlaForeach.java
+++ src/petlaForeach.java
@@ -9,5 +9,17 @@ public class petlaForeach {
         //  price2 = price2 *1.2;
             System.out.print(price2*1.1+", ");
         }
+        System.out.println();
+        System.out.println("----Zadanie 2-------------------------");
+        System.out.println();
+
+        int[]numbers={1,2,3,5,23,15,23,10,100,22};
+
+
+        for (int five : numbers){
+            if (five%5==0){
+                System.out.print(five+", ");
+            }
+        }
     }
 }
